71fa8a98f58dffbea831148a18a7840582000faa
Yoav Ram <yoavram+github@gmail.com>
2016-02-08 11:01:13 +0200

    many session; moved html to www; added data

---
+++ scripts/example_module.py
@@ -0,0 +1,15 @@
+"""module docstring"""
+
+# imports
+# constants
+# exception classes
+# interface functions
+# classes
+# internal functions & classes
+
+def main(...):
+    ...
+
+if __name__ == '__main__':
+    status = main()
+    sys.exit(status)
