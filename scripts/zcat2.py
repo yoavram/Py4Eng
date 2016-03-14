c0b808bf7150179673055fe2e552f66eab706a3e
Yoav Ram <yoavram+github@gmail.com>
2016-03-14 17:59:15 +0200

    zcat using click

---
+++ scripts/zcat2.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Extract a gzipped file and print its contents.
+
+Created on Tue Feb  9 12:30:45 2016
+
+@author: yoav@yoavram.com
+"""
+import sys
+import os
+import gzip
+import click
+
+@click.command()
+@click.argument('filename', type=click.Path(exists=True, readable=True))
+def zcat(filename):
+    with gzip.open(filename, mode='rt', encoding='utf8') as f:        
+        print(f.read())
+
+if __name__ == '__main__':
+    zcat()
