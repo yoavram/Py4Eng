f104fd022d659e7adf8c9ee7db8a86a4f30dbbc1
Yoav Ram <yoavram+github@gmail.com>
2016-04-04 22:53:34 +0300

    formatting

--- scripts/ctypes/fibonacci.py
+++ scripts/ctypes/fibonacci.py
@@ -8,39 +8,47 @@ Demonstrates the use of ctypes with three functions:
     (3) fibmatrix(c)
 
 See: https://scipy-cookbook.readthedocs.org/items/Ctypes.html#fibonacci-example-using-numpy-arrays-c-and-scons
+     http://www.lejordet.com/2009/04/simple-python-ctypes-example-in-windows/
 """
-
 import numpy as np
 import ctypes as ct
 
 # Load the library as _libfibonacci.
 # Why the underscore (_) in front of _libfibonacci below?
-# To mimimise namespace pollution -- see PEP 8 (www.python.org).
+# To minimise namespace pollution -- see PEP 8 (www.python.org).
 _libfibonacci = np.ctypeslib.load_library('fibonacci.dll', '.')
 
 _libfibonacci.fib.argtypes = [ct.c_int] #  Declare arg type, same below.
 _libfibonacci.fib.restype  =  ct.c_int  #  Declare result type, same below.
 
-_libfibonacci.fibseries.argtypes = [np.ctypeslib.ndpointer(dtype = np.int),\
-                                     ct.c_int,\
-                                     np.ctypeslib.ndpointer(dtype = np.int)]
-_libfibonacci.fibseries.restype  = ct.c_void_p
+_libfibonacci.fibseries.argtypes = [
+    np.ctypeslib.ndpointer(dtype=np.int),
+    ct.c_int,
+    np.ctypeslib.ndpointer(dtype=np.int)
+]
+_libfibonacci.fibseries.restype = ct.c_void_p
 
-_libfibonacci.fibmatrix.argtypes = [np.ctypeslib.ndpointer(dtype = np.int),\
-                                     ct.c_int, ct.c_int,\
-                                    np.ctypeslib.ndpointer(dtype = np.int)]
-_libfibonacci.fibmatrix.restype  = ct.c_void_p
+_libfibonacci.fibmatrix.argtypes = [
+    np.ctypeslib.ndpointer(dtype=np.int),
+    ct.c_int, ct.c_int,
+    np.ctypeslib.ndpointer(dtype=np.int)
+]
+_libfibonacci.fibmatrix.restype = ct.c_void_p
 
 def fib(a):
     """Compute the n'th Fibonacci number.
 
-    ARGUMENT(S):
-        An integer.
+    Arguments
+    ---------
+    a : int
 
-    RESULT(S):
+    Returns
+    -------
+    int
         The n'th Fibonacci number.
 
-    EXAMPLE(S):
+    Examples
+    --------
     >>> fib(8)
     13
     >>> fib(23)
@@ -53,16 +61,21 @@ def fib(a):
 def fibseries(b):
     """Compute an array containing the n'th Fibonacci number of each entry.
 
-    ARGUMENT(S):
-        A list or NumPy array (dim = 1) of integers.
+    Arguments
+    ---------
+    b : list or numpy.ndarray
+        a sequence of integers
 
-    RESULT(S):
-        NumPy array containing the n'th Fibonacci number of each entry.
+    Returns
+    -------
+    numpy.ndarray
+        array containing the n'th Fibonacci number of each entry
 
-    EXAMPLE(S):
-    >>> fibseries([1,2,3,4,5,6,7,8])
+    Examples
+    --------
+    >>> fibseries([1, 2, 3, 4, 5, 6, 7, 8])
     array([ 0,  1,  1,  2,  3,  5,  8, 13])
-    >>> fibseries(range(1,12))
+    >>> fibseries(range(1, 12))
     array([ 0,  1,  1,  2,  3,  5,  8, 13, 21, 34, 55])
     """
     b = np.asarray(b, dtype=np.intc)
@@ -73,13 +86,17 @@ def fibseries(b):
 def fibmatrix(c):
     """Compute a matrix containing the n'th Fibonacci number of each entry.
 
-    ARGUMENT(S):
-        A nested list or NumPy array (dim = 2) of integers.
+    Arguments
+    ---------
+    c : list or numpy.ndarray
+        A nested list or 2D NumPy array of integers
 
-    RESULT(S):
-        NumPy array containing the n'th Fibonacci number of each entry.
+    Returns
+    -------
+        NumPy array containing the n'th Fibonacci number of each entry
 
-    EXAMPLE(S):
+    Examples
+    --------
     >>> from numpy import array
     >>> fibmatrix([[3,4],[5,6]])
     array([[1, 2],
