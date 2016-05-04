eba15a8ff17d3d815609c5300657890176da8c5e
Yoav Ram <yoavram+github@gmail.com>
2016-05-04 08:18:47 +0300

    fix indent

--- solutions/pid.py
+++ solutions/pid.py
@@ -9,8 +9,8 @@ if __name__ == '__main__':
     print("Siblings:")
     with concurrent.futures.ProcessPoolExecutor() as executor:
         futures = [executor.submit(pid) for _ in range(10)]
-    for future in futures:
-        if future.exception():
-            print(future.exception())
-        else:
-            print('{1}->{0}'.format(*future.result()))
+        for future in futures:
+            if future.exception():
+                print(future.exception())
+            else:
+                print('{1}->{0}'.format(*future.result()))
