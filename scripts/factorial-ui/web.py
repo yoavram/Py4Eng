4074a0bb8ac0d4a88f91ccbe9913a050f7f00618
Yoav Ram <yoavram+github@gmail.com>
2016-07-05 19:22:30 +0300

    load factorial from cli, typo

--- scripts/factorial-ui/web.py
+++ scripts/factorial-ui/web.py
@@ -7,30 +7,19 @@ import os
 import json
 from flask import Flask, jsonify
 
-
-def factorial(n):
-	'''Calculate the factorial of n using recursion.
-	'''
-	if n < 1:
-		return 1
-	else:
-		return n * factorial(n-1)
-
+from cli import factorial
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def root():
 	return app.send_static_file('index.html')
 
-
 @app.route('/<int:n>')
 def calc(n=10):
-	result = factorial(n)
-	return jsonify(result=result)
-
+	n_factorial = factorial(n)
+	return jsonify(result=n_factorial)
 
 if __name__=='__main__':
-	app.run(host='127.0.0.1', port=5000, debug=True)
-	app.logger.info("Server shuting down")
+	app.run(host='127.0.0.1', port=5000, debug=False)
+	app.logger.info("Server shutting down")
