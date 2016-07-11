7f1e5d41a08f3536bc13616c7108a9363b9045bb
Yoav Ram <yoavram+github@gmail.com>
2016-07-11 14:16:15 +0300

    Revert "load factorial from cli, typo"
    This reverts commit 4074a0bb8ac0d4a88f91ccbe9913a050f7f00618.

--- scripts/factorial-ui/web.py
+++ scripts/factorial-ui/web.py
@@ -7,19 +7,30 @@ import os
 import json
 from flask import Flask, jsonify
 
-from cli import factorial
+
+def factorial(n):
+	'''Calculate the factorial of n using recursion.
+	'''
+	if n < 1:
+		return 1
+	else:
+		return n * factorial(n-1)
+
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def root():
 	return app.send_static_file('index.html')
 
+
 @app.route('/<int:n>')
 def calc(n=10):
-	n_factorial = factorial(n)
-	return jsonify(result=n_factorial)
+	result = factorial(n)
+	return jsonify(result=result)
+
 
 if __name__=='__main__':
-	app.run(host='127.0.0.1', port=5000, debug=False)
-	app.logger.info("Server shutting down")
+	app.run(host='127.0.0.1', port=5000, debug=True)
+	app.logger.info("Server shuting down")
